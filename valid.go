61c97a2a4f6506a19094d2d10f6fc2ac0050d3e3
Faye Amacker <fx.amacker@gmail.com>
2019-10-01 17:59:20 -0500

    Improve gocyclo score to 80% (was 70%)
    Reduce checkValid() cyclomatic complexity from 33 to 14.

--- valid.go
+++ valid.go
@@ -26,6 +26,9 @@ func (e *SemanticError) Error() string { return e.msg }
 
 // Valid checks whether CBOR data is complete and well-formed.
 func Valid(data []byte) (rest []byte, err error) {
+	if len(data) == 0 {
+		return nil, io.EOF
+	}
 	offset, _, err := checkValid(data, 0)
 	if err != nil {
 		return nil, err
@@ -35,74 +38,33 @@ func Valid(data []byte) (rest []byte, err error) {
 
 func checkValid(data []byte, off int) (_ int, t cborType, err error) {
 	if len(data)-off < 1 {
-		return 0, 0, io.EOF
-	}
-	t = cborType(data[off] & 0xE0)
-	ai := data[off] & 0x1F
-	val := uint64(ai)
-	off++
-
-	// Check additional information.
-	switch ai {
-	case 24:
-		if len(data)-off < 1 {
-			return 0, 0, io.ErrUnexpectedEOF
-		}
-		val = uint64(data[off])
-		off++
-	case 25:
-		if len(data)-off < 2 {
-			return 0, 0, io.ErrUnexpectedEOF
-		}
-		val = uint64(binary.BigEndian.Uint16(data[off : off+2]))
-		off += 2
-	case 26:
-		if len(data)-off < 4 {
-			return 0, 0, io.ErrUnexpectedEOF
-		}
-		val = uint64(binary.BigEndian.Uint32(data[off : off+4]))
-		off += 4
-	case 27:
-		if len(data)-off < 8 {
-			return 0, 0, io.ErrUnexpectedEOF
-		}
-		val = binary.BigEndian.Uint64(data[off : off+8])
-		off += 8
-	case 28, 29, 30:
-		return 0, 0, &SyntaxError{"cbor: invalid additional information " + strconv.Itoa(int(ai)) + " for type " + t.String()}
-	case 31:
-		switch t {
-		case cborTypePositiveInt, cborTypeNegativeInt, cborTypeTag:
-			return 0, 0, &SyntaxError{"cbor: invalid additional information " + strconv.Itoa(int(ai)) + " for type " + t.String()}
-		}
+		return 0, 0, io.ErrUnexpectedEOF
 	}
 
-	// Check indefinite byte/text string, array, and map.
-	if ai == 31 {
-		switch t {
-		case cborTypeByteString, cborTypeTextString, cborTypeArray, cborTypeMap:
-			if off, err = checkValidIndefinite(data, off, t); err != nil {
-				return 0, 0, err
-			}
-			return off, t, nil
-		}
+	off, t, val, indefinite, err := checkTypeAndValue(data, off)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	if indefinite {
+		off, err = checkValidIndefinite(data, off, t)
+		return off, t, err
 	}
 
 	switch t {
-	case cborTypeByteString, cborTypeTextString: // Check byte/text string payload.
+	case cborTypeByteString, cborTypeTextString:
 		valInt := int(val)
-		if valInt < 0 || uint64(valInt) != val {
+		if valInt < 0 {
 			// Detect integer overflow
 			return 0, 0, errors.New("cbor: " + t.String() + " length " + strconv.FormatUint(val, 10) + " is too large, causing integer overflow")
 		}
 		if len(data)-off < valInt {
 			return 0, 0, io.ErrUnexpectedEOF
 		}
-		off += int(val)
-
-	case cborTypeArray, cborTypeMap: // Check array and map payload.
+		off += valInt
+	case cborTypeArray, cborTypeMap:
 		valInt := int(val)
-		if valInt < 0 || uint64(valInt) != val {
+		if valInt < 0 {
 			// Detect integer overflow
 			return 0, 0, errors.New("cbor: " + t.String() + " length " + strconv.FormatUint(val, 10) + " is too large, causing integer overflow")
 		}
@@ -110,27 +72,15 @@ func checkValid(data []byte, off int) (_ int, t cborType, err error) {
 		if t == cborTypeMap {
 			elementCount = 2
 		}
-		for i := 0; i < elementCount; i++ {
+		for j := 0; j < elementCount; j++ {
 			for i := 0; i < valInt; i++ {
 				if off, _, err = checkValid(data, off); err != nil {
-					if err == io.EOF {
-						err = io.ErrUnexpectedEOF
-					}
 					return 0, 0, err
 				}
 			}
 		}
 	case cborTypeTag: // Check tagged item following tag.
-		if off, t, err = checkValid(data, off); err != nil {
-			if err == io.EOF {
-				err = io.ErrUnexpectedEOF
-			}
-			return 0, 0, err
-		}
-	case cborTypePrimitives: // 0xFF (break code) should not be outside checkValidIndefinite().
-		if ai == 31 {
-			return 0, 0, &SyntaxError{"cbor: unexpected \"break\" code"}
-		}
+		return checkValid(data, off)
 	}
 	return off, t, nil
 }
@@ -155,12 +105,55 @@ func checkValidIndefinite(data []byte, off int, t cborType) (_ int, err error) {
 			}
 		case cborTypeMap:
 			if off, _, err = checkValid(data, off); err != nil {
-				if err == io.EOF {
-					err = io.ErrUnexpectedEOF
-				}
 				return 0, err
 			}
 		}
 	}
 	return off, nil
+}
+
+func checkTypeAndValue(data []byte, off int) (_ int, t cborType, val uint64, indefinite bool, err error) {
+	t = cborType(data[off] & 0xE0)
+	ai := data[off] & 0x1F
+	val = uint64(ai)
+	off++
+
+	switch ai {
+	case 24:
+		if len(data)-off < 1 {
+			return 0, 0, 0, false, io.ErrUnexpectedEOF
+		}
+		val = uint64(data[off])
+		off++
+	case 25:
+		if len(data)-off < 2 {
+			return 0, 0, 0, false, io.ErrUnexpectedEOF
+		}
+		val = uint64(binary.BigEndian.Uint16(data[off : off+2]))
+		off += 2
+	case 26:
+		if len(data)-off < 4 {
+			return 0, 0, 0, false, io.ErrUnexpectedEOF
+		}
+		val = uint64(binary.BigEndian.Uint32(data[off : off+4]))
+		off += 4
+	case 27:
+		if len(data)-off < 8 {
+			return 0, 0, 0, false, io.ErrUnexpectedEOF
+		}
+		val = binary.BigEndian.Uint64(data[off : off+8])
+		off += 8
+	case 28, 29, 30:
+		return 0, 0, 0, false, &SyntaxError{"cbor: invalid additional information " + strconv.Itoa(int(ai)) + " for type " + t.String()}
+	case 31:
+		switch t {
+		case cborTypePositiveInt, cborTypeNegativeInt, cborTypeTag:
+			return 0, 0, 0, false, &SyntaxError{"cbor: invalid additional information " + strconv.Itoa(int(ai)) + " for type " + t.String()}
+		case cborTypePrimitives: // 0xFF (break code) should not be outside checkValidIndefinite().
+			return 0, 0, 0, false, &SyntaxError{"cbor: unexpected \"break\" code"}
+		case cborTypeByteString, cborTypeTextString, cborTypeArray, cborTypeMap:
+			return off, t, val, true, nil
+		}
+	}
+	return off, t, val, false, nil
 }
