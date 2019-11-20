2964577e9adaf2351ff0bc8fcb4d6d4498bdc1da
Faye Amacker <33205765+fxamacker@users.noreply.github.com>
2019-11-19 22:20:44 -0600

    Add toarray and keyasint examples

--- example_test.go
+++ example_test.go
@@ -50,7 +50,7 @@ func ExampleMarshal_time() {
 }
 
 // This example uses Marshal to encode struct and map in canonical form.
-func ExampleMarshal_structAndMapCanonical() {
+func ExampleMarshal_canonical() {
 	type Animal struct {
 		Age      int
 		Name     string
@@ -67,6 +67,42 @@ func ExampleMarshal_structAndMapCanonical() {
 	// a46341676504644d616c65f4644e616d656543616e647968436f6e7461637473a2634a6f656c3232322d3232322d32323232644d6172796c3131312d3131312d31313131
 }
 
+// This example uses "toarray" struct tag to encode struct as CBOR array.
+func ExampleMarshal_toarray() {
+	type Record struct {
+		_           struct{} `cbor:",toarray"`
+		Name        string
+		Unit        string
+		Measurement int
+	}
+	rec := Record{Name: "current", Unit: "V", Measurement: 1}
+	b, err := cbor.Marshal(rec, cbor.EncOptions{})
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	fmt.Printf("%0x\n", b)
+	// Output:
+	// 836763757272656e74615601
+}
+
+// This example uses "keyasint" struct tag to encode struct's fiele names as integer.
+// This feautre is very useful in handling COSE, CWT, SenML data.
+func ExampleMarshal_keyasint() {
+	type Record struct {
+		Name        string `cbor:"1,keyasint"`
+		Unit        string `cbor:"2,keyasint"`
+		Measurement int    `cbor:"3,keyasint"`
+	}
+	rec := Record{Name: "current", Unit: "V", Measurement: 1}
+	b, err := cbor.Marshal(rec, cbor.EncOptions{})
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	fmt.Printf("%0x\n", b)
+	// Output:
+	// a3016763757272656e740261560301
+}
+
 func ExampleUnmarshal() {
 	type Animal struct {
 		Age    int
@@ -74,8 +110,7 @@ func ExampleUnmarshal() {
 		Owners []string
 		Male   bool
 	}
-	cborHex := "a46341676504644e616d656543616e6479664f776e65727382644d617279634a6f65644d616c65f4"
-	cborData, _ := hex.DecodeString(cborHex)
+	cborData, _ := hex.DecodeString("a46341676504644e616d656543616e6479664f776e65727382644d617279634a6f65644d616c65f4")
 	var animal Animal
 	err := cbor.Unmarshal(cborData, &animal)
 	if err != nil {
@@ -87,13 +122,13 @@ func ExampleUnmarshal() {
 }
 
 func ExampleUnmarshal_time() {
-	cborRFC3339Time := hexDecode("74323031332d30332d32315432303a30343a30305a")
-	cborUnixTime := hexDecode("1a514b67b0")
+	cborRFC3339Time, _ := hex.DecodeString("74323031332d30332d32315432303a30343a30305a")
 	tm := time.Time{}
 	if err := cbor.Unmarshal(cborRFC3339Time, &tm); err != nil {
 		fmt.Println("error:", err)
 	}
 	fmt.Printf("%+v\n", tm.UTC().Format(time.RFC3339Nano))
+	cborUnixTime, _ := hex.DecodeString("1a514b67b0")
 	tm = time.Time{}
 	if err := cbor.Unmarshal(cborUnixTime, &tm); err != nil {
 		fmt.Println("error:", err)
@@ -127,32 +162,6 @@ func ExampleEncoder() {
 	fmt.Printf("%0x\n", buf.Bytes())
 	// Output:
 	// a46341676504644d616c65f4644e616d656543616e6479664f776e65727382644d617279634a6f65a46341676506644d616c65f5644e616d656452756479664f776e657273816543696e6479a46341676502644d616c65f5644e616d656444756b65664f776e65727381664e6f72746f6e
-}
-
-func ExampleDecoder() {
-	type Animal struct {
-		Age    int
-		Name   string
-		Owners []string
-		Male   bool
-	}
-	cborHex := "a46341676504644d616c65f4644e616d656543616e6479664f776e65727382644d617279634a6f65a46341676506644d616c65f5644e616d656452756479664f776e657273816543696e6479a46341676502644d616c65f5644e616d656444756b65664f776e65727381664e6f72746f6e"
-	cborData, _ := hex.DecodeString(cborHex)
-	dec := cbor.NewDecoder(bytes.NewReader(cborData))
-	for {
-		var animal Animal
-		if err := dec.Decode(&animal); err != nil {
-			if err != io.EOF {
-				fmt.Println("error:", err)
-			}
-			break
-		}
-		fmt.Printf("%+v\n", animal)
-	}
-	// Output:
-	// {Age:4 Name:Candy Owners:[Mary Joe] Male:false}
-	// {Age:6 Name:Rudy Owners:[Cindy] Male:true}
-	// {Age:2 Name:Duke Owners:[Norton] Male:true}
 }
 
 // ExampleEncoder_indefiniteLengthByteString encodes a stream of definite
@@ -295,8 +304,33 @@ func ExampleEncoder_indefiniteLengthMap() {
 	// bf61610161629f0203ffff
 }
 
+func ExampleDecoder() {
+	type Animal struct {
+		Age    int
+		Name   string
+		Owners []string
+		Male   bool
+	}
+	cborData, _ := hex.DecodeString("a46341676504644d616c65f4644e616d656543616e6479664f776e65727382644d617279634a6f65a46341676506644d616c65f5644e616d656452756479664f776e657273816543696e6479a46341676502644d616c65f5644e616d656444756b65664f776e65727381664e6f72746f6e")
+	dec := cbor.NewDecoder(bytes.NewReader(cborData))
+	for {
+		var animal Animal
+		if err := dec.Decode(&animal); err != nil {
+			if err != io.EOF {
+				fmt.Println("error:", err)
+			}
+			break
+		}
+		fmt.Printf("%+v\n", animal)
+	}
+	// Output:
+	// {Age:4 Name:Candy Owners:[Mary Joe] Male:false}
+	// {Age:6 Name:Rudy Owners:[Cindy] Male:true}
+	// {Age:2 Name:Duke Owners:[Norton] Male:true}
+}
+
 func Example_cWT() {
-	// Use "keyasint" struct tag to encode/decode struct from/to CBOR map.
+	// Use "keyasint" struct tag to encode/decode struct to/from CBOR map.
 	type claims struct {
 		Iss string `cbor:"1,keyasint"`
 		Sub string `cbor:"2,keyasint"`
@@ -307,7 +341,7 @@ func Example_cWT() {
 		Cti []byte `cbor:"7,keyasint"`
 	}
 	// Data from https://tools.ietf.org/html/rfc8392#appendix-A section A.1
-	cborData := hexDecode("a70175636f61703a2f2f61732e6578616d706c652e636f6d02656572696b77037818636f61703a2f2f6c696768742e6578616d706c652e636f6d041a5612aeb0051a5610d9f0061a5610d9f007420b71")
+	cborData, _ := hex.DecodeString("a70175636f61703a2f2f61732e6578616d706c652e636f6d02656572696b77037818636f61703a2f2f6c696768742e6578616d706c652e636f6d041a5612aeb0051a5610d9f0061a5610d9f007420b71")
 	var v claims
 	if err := cbor.Unmarshal(cborData, &v); err != nil {
 		fmt.Println("error:", err)
@@ -321,14 +355,14 @@ func Example_cWT() {
 }
 
 func Example_signedCWT() {
-	// Use "keyasint" struct tag to encode/decode struct from/to CBOR map.
+	// Use "keyasint" struct tag to encode/decode struct to/from CBOR map.
 	// Partial COSE header definition
 	type coseHeader struct {
 		Alg int    `cbor:"1,keyasint,omitempty"`
 		Kid []byte `cbor:"4,keyasint,omitempty"`
 		IV  []byte `cbor:"5,keyasint,omitempty"`
 	}
-	// Use "toarray" struct tag to encode/decode struct from/to CBOR array.
+	// Use "toarray" struct tag to encode/decode struct to/from CBOR array.
 	type signedCWT struct {
 		_           struct{} `cbor:",toarray"`
 		Protected   []byte
@@ -337,7 +371,7 @@ func Example_signedCWT() {
 		Signature   []byte
 	}
 	// Data from https://tools.ietf.org/html/rfc8392#appendix-A section A.3
-	cborData := hexDecode("d28443a10126a104524173796d6d657472696345434453413235365850a70175636f61703a2f2f61732e6578616d706c652e636f6d02656572696b77037818636f61703a2f2f6c696768742e6578616d706c652e636f6d041a5612aeb0051a5610d9f0061a5610d9f007420b7158405427c1ff28d23fbad1f29c4c7c6a555e601d6fa29f9179bc3d7438bacaca5acd08c8d4d4f96131680c429a01f85951ecee743a52b9b63632c57209120e1c9e30")
+	cborData, _ := hex.DecodeString("d28443a10126a104524173796d6d657472696345434453413235365850a70175636f61703a2f2f61732e6578616d706c652e636f6d02656572696b77037818636f61703a2f2f6c696768742e6578616d706c652e636f6d041a5612aeb0051a5610d9f0061a5610d9f007420b7158405427c1ff28d23fbad1f29c4c7c6a555e601d6fa29f9179bc3d7438bacaca5acd08c8d4d4f96131680c429a01f85951ecee743a52b9b63632c57209120e1c9e30")
 	var v signedCWT
 	if err := cbor.Unmarshal(cborData, &v); err != nil {
 		fmt.Println("error:", err)
@@ -351,7 +385,7 @@ func Example_signedCWT() {
 }
 
 func Example_cOSE() {
-	// Use "keyasint" struct tag to encode/decode struct from/to CBOR map.
+	// Use "keyasint" struct tag to encode/decode struct to/from CBOR map.
 	// Use cbor.RawMessage to delay unmarshaling (CrvOrNOrK's data type depends on Kty's value).
 	type coseKey struct {
 		Kty       int             `cbor:"1,keyasint,omitempty"`
@@ -366,7 +400,7 @@ func Example_cOSE() {
 	}
 	// Data from https://tools.ietf.org/html/rfc8392#appendix-A section A.2
 	// 128-Bit Symmetric Key
-	cborData := hexDecode("a42050231f4c4d4d3051fdc2ec0a3851d5b3830104024c53796d6d6574726963313238030a")
+	cborData, _ := hex.DecodeString("a42050231f4c4d4d3051fdc2ec0a3851d5b3830104024c53796d6d6574726963313238030a")
 	var v coseKey
 	if err := cbor.Unmarshal(cborData, &v); err != nil {
 		fmt.Println("error:", err)
@@ -380,7 +414,7 @@ func Example_cOSE() {
 }
 
 func Example_senML() {
-	// Use "keyasint" struct tag to encode/decode struct from/to CBOR map.
+	// Use "keyasint" struct tag to encode/decode struct to/from CBOR map.
 	type SenMLRecord struct {
 		BaseName    string  `cbor:"-2,keyasint,omitempty"`
 		BaseTime    float64 `cbor:"-3,keyasint,omitempty"`
@@ -399,7 +433,7 @@ func Example_senML() {
 		Sum         float64 `cbor:"5,keyasint,omitempty"`
 	}
 	// Data from https://tools.ietf.org/html/rfc8428#section-6
-	cborData := hexDecode("87a721781b75726e3a6465763a6f773a3130653230373361303130383030363a22fb41d303a15b00106223614120050067766f6c7461676501615602fb405e066666666666a3006763757272656e74062402fb3ff3333333333333a3006763757272656e74062302fb3ff4cccccccccccda3006763757272656e74062202fb3ff6666666666666a3006763757272656e74062102f93e00a3006763757272656e74062002fb3ff999999999999aa3006763757272656e74060002fb3ffb333333333333")
+	cborData, _ := hex.DecodeString("87a721781b75726e3a6465763a6f773a3130653230373361303130383030363a22fb41d303a15b00106223614120050067766f6c7461676501615602fb405e066666666666a3006763757272656e74062402fb3ff3333333333333a3006763757272656e74062302fb3ff4cccccccccccda3006763757272656e74062202fb3ff6666666666666a3006763757272656e74062102f93e00a3006763757272656e74062002fb3ff999999999999aa3006763757272656e74060002fb3ffb333333333333")
 	var v []SenMLRecord
 	if err := cbor.Unmarshal(cborData, &v); err != nil {
 		fmt.Println("error:", err)
@@ -427,7 +461,7 @@ func Example_webAuthn() {
 		Fmt       string          `cbor:"fmt"`
 		AttStmt   cbor.RawMessage `cbor:"attStmt"`
 	}
-	cborData := hexDecode("a363666d74686669646f2d7532666761747453746d74a26373696758483046022100e7ab373cfbd99fcd55fd59b0f6f17fef5b77a20ddec3db7f7e4d55174e366236022100828336b4822125fb56541fb14a8a273876acd339395ec2dad95cf41c1dd2a9ae637835638159024e3082024a30820132a0030201020204124a72fe300d06092a864886f70d01010b0500302e312c302a0603550403132359756269636f2055324620526f6f742043412053657269616c203435373230303633313020170d3134303830313030303030305a180f32303530303930343030303030305a302c312a302806035504030c2159756269636f205532462045452053657269616c203234393431343937323135383059301306072a8648ce3d020106082a8648ce3d030107034200043d8b1bbd2fcbf6086e107471601468484153c1c6d3b4b68a5e855e6e40757ee22bcd8988bf3befd7cdf21cb0bf5d7a150d844afe98103c6c6607d9faae287c02a33b3039302206092b0601040182c40a020415312e332e362e312e342e312e34313438322e312e313013060b2b0601040182e51c020101040403020520300d06092a864886f70d01010b05000382010100a14f1eea0076f6b8476a10a2be72e60d0271bb465b2dfbfc7c1bd12d351989917032631d795d097fa30a26a325634e85721bc2d01a86303f6bc075e5997319e122148b0496eec8d1f4f94cf4110de626c289443d1f0f5bbb239ca13e81d1d5aa9df5af8e36126475bfc23af06283157252762ff68879bcf0ef578d55d67f951b4f32b63c8aea5b0f99c67d7d814a7ff5a6f52df83e894a3a5d9c8b82e7f8bc8daf4c80175ff8972fda79333ec465d806eacc948f1bab22045a95558a48c20226dac003d41fbc9e05ea28a6bb5e10a49de060a0a4f6a2676a34d68c4abe8c61874355b9027e828ca9e064b002d62e8d8cf0744921753d35e3c87c5d5779453e7768617574684461746158c449960de5880e8c687434170f6476605b8fe4aeb9a28632c7995cf3ba831d976341000000000000000000000000000000000000000000408903fd7dfd2c9770e98cae0123b13a2c27828a106349bc6277140e7290b7e9eb7976aa3c04ed347027caf7da3a2fa76304751c02208acfc4e7fc6c7ebbc375c8a5010203262001215820ad7f7992c335b90d882b2802061b97a4fabca7e2ee3e7a51e728b8055e4eb9c7225820e0966ba7005987fece6f0e0e13447aa98cec248e4000a594b01b74c1cb1d40b3")
+	cborData, _ := hex.DecodeString("a363666d74686669646f2d7532666761747453746d74a26373696758483046022100e7ab373cfbd99fcd55fd59b0f6f17fef5b77a20ddec3db7f7e4d55174e366236022100828336b4822125fb56541fb14a8a273876acd339395ec2dad95cf41c1dd2a9ae637835638159024e3082024a30820132a0030201020204124a72fe300d06092a864886f70d01010b0500302e312c302a0603550403132359756269636f2055324620526f6f742043412053657269616c203435373230303633313020170d3134303830313030303030305a180f32303530303930343030303030305a302c312a302806035504030c2159756269636f205532462045452053657269616c203234393431343937323135383059301306072a8648ce3d020106082a8648ce3d030107034200043d8b1bbd2fcbf6086e107471601468484153c1c6d3b4b68a5e855e6e40757ee22bcd8988bf3befd7cdf21cb0bf5d7a150d844afe98103c6c6607d9faae287c02a33b3039302206092b0601040182c40a020415312e332e362e312e342e312e34313438322e312e313013060b2b0601040182e51c020101040403020520300d06092a864886f70d01010b05000382010100a14f1eea0076f6b8476a10a2be72e60d0271bb465b2dfbfc7c1bd12d351989917032631d795d097fa30a26a325634e85721bc2d01a86303f6bc075e5997319e122148b0496eec8d1f4f94cf4110de626c289443d1f0f5bbb239ca13e81d1d5aa9df5af8e36126475bfc23af06283157252762ff68879bcf0ef578d55d67f951b4f32b63c8aea5b0f99c67d7d814a7ff5a6f52df83e894a3a5d9c8b82e7f8bc8daf4c80175ff8972fda79333ec465d806eacc948f1bab22045a95558a48c20226dac003d41fbc9e05ea28a6bb5e10a49de060a0a4f6a2676a34d68c4abe8c61874355b9027e828ca9e064b002d62e8d8cf0744921753d35e3c87c5d5779453e7768617574684461746158c449960de5880e8c687434170f6476605b8fe4aeb9a28632c7995cf3ba831d976341000000000000000000000000000000000000000000408903fd7dfd2c9770e98cae0123b13a2c27828a106349bc6277140e7290b7e9eb7976aa3c04ed347027caf7da3a2fa76304751c02208acfc4e7fc6c7ebbc375c8a5010203262001215820ad7f7992c335b90d882b2802061b97a4fabca7e2ee3e7a51e728b8055e4eb9c7225820e0966ba7005987fece6f0e0e13447aa98cec248e4000a594b01b74c1cb1d40b3")
 	var v attestationObject
 	if err := cbor.Unmarshal(cborData, &v); err != nil {
 		fmt.Println("error:", err)
