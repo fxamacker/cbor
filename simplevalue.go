2e7844124ae09cb451d66b8e95e5c04a2be577ae
Faye Amacker <33205765+fxamacker@users.noreply.github.com>
2022-11-13 18:42:09 -0600

    Add comment describing CBOR simple value

--- simplevalue.go
+++ simplevalue.go
@@ -2,7 +2,14 @@ package cbor
 
 import "reflect"
 
-// SimpleValue represents CBOR simple value [0, 255].
+// SimpleValue represents CBOR simple value.
+// CBOR simple value is:
+// * an extension point like CBOR tag.
+// * a subset of CBOR major type 7 that isn't floating-point.
+// * "identified by a number between 0 and 255, but distinct from that number itself".
+//   For example, "a simple value 2 is not equivalent to an integer 2" as a CBOR map key.
+// CBOR simple values identified by 20..23 are: "false", "true" , "null", and "undefined".
+// Other CBOR simple values are currently unassigned/reserved by IANA.
 type SimpleValue uint8
 
 var (
