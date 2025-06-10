83e4f58a2c99e9489301e196c99366e3a0dbc0d4
Ben Luddy <bluddy@redhat.com>
2025-06-10 14:02:27 -0400

    Add optional encoding support for json.Marshaler.
    If the user provides a JSON-to-CBOR transcode function, a value whose type implements json.Marshaler
    and not cbor.Marshaler will be encoded by first calling its MarshalJSON method, then transcoding the
    result to CBOR.
    
    Signed-off-by: Ben Luddy <bluddy@redhat.com>

--- json_test.go
+++ json_test.go
@@ -5,11 +5,65 @@ package cbor_test
 
 import (
 	"encoding/json"
+	"fmt"
+	"io"
 	"reflect"
 	"testing"
 
 	"github.com/fxamacker/cbor/v2"
 )
+
+type TranscoderFunc func(io.Writer, io.Reader) error
+
+func (f TranscoderFunc) Transcode(w io.Writer, r io.Reader) error {
+	return f(w, r)
+}
+
+func ExampleTranscoder_fromJSON() {
+	enc, _ := cbor.EncOptions{
+		JSONMarshalerTranscoder: TranscoderFunc(func(w io.Writer, r io.Reader) error {
+			d := json.NewDecoder(r)
+
+			for {
+				token, err := d.Token()
+				if err == io.EOF {
+					return nil
+				}
+				if err != nil {
+					return err
+				}
+				switch token {
+				case json.Delim('['):
+					if _, err := w.Write([]byte{0x9f}); err != nil {
+						return err
+					}
+				case json.Delim('{'):
+					if _, err := w.Write([]byte{0xbf}); err != nil {
+						return err
+					}
+				case json.Delim(']'), json.Delim('}'):
+					if _, err := w.Write([]byte{0xff}); err != nil {
+						return err
+					}
+				default:
+					b, err := cbor.Marshal(token)
+					if err != nil {
+						return err
+					}
+					if _, err := w.Write(b); err != nil {
+						return err
+					}
+				}
+			}
+		}),
+	}.EncMode()
+
+	got, _ := enc.Marshal(json.RawMessage(`{"a": [true, "z", {"y": 3.14}], "b": {"c": null}}`))
+	diag, _ := cbor.Diagnose(got)
+	fmt.Println(diag)
+	// Output: {_ "a": [_ true, "z", {_ "y": 3.14}], "b": {_ "c": null}}
+
+}
 
 // TestStdlibJSONCompatibility tests compatibility as a drop-in replacement for the standard library
 // encoding/json package on a round trip encoding from Go object to interface{}.
